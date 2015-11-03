f79b3eb98522ff668aed25401cdd22a2ef0d767a
Alessandra Pascazio <alemidori777@gmail.com>
2015-11-03 17:17:37 +0100

    apertura file json e lettura (no parsing)

--- app/src/main/java/com/example/midori/writer/MainController.java
+++ app/src/main/java/com/example/midori/writer/MainController.java
@@ -1,7 +1,5 @@
 package com.example.midori.writer;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
--- app/src/main/java/com/example/midori/writer/RootActivity.java
+++ app/src/main/java/com/example/midori/writer/RootActivity.java
@@ -2,14 +2,16 @@ package com.example.midori.writer;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 
@@ -18,7 +20,6 @@ public class RootActivity extends Activity {
     private static RootActivity rootActivity;
     private static Context context;
     private static SafeButton selectableButton, nextButton;
-    private static List<CharSequence> selectableButtons;
     private static TextView topText;
     private static int layoutValue;
 
@@ -57,6 +58,15 @@ public class RootActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+        InputStream is = this.getResources().openRawResource(R.raw.tree_structure);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        try {
+            System.out.println(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         rootActivity = this;
         context = this.getApplicationContext();
 
@@ -71,6 +81,7 @@ public class RootActivity extends Activity {
                 break;
             case 2:
                 setContentView(R.layout.four_buttons);
+                nextButton = (SafeButton) findViewById(R.id.button2);
                 break;
             case 3:
                 setContentView(R.layout.eight_buttons);
